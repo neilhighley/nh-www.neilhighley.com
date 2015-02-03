3265d8ccfd866e078791a48cbd677a7f1657572d
Neil Highley <neil@neilhighley.com>
2015-02-03 00:05:46 +0000

    site update

--- website/app/d-portfolioItem.js
+++ website/app/d-portfolioItem.js
@@ -9,17 +9,17 @@ nhDirectives.directive("portfolioItem",[function(){
     }
   function CreateHTML(data){
         var t="";
-        t+="<div class='pi-item col-md-6'>";
+        t+="<div class='pi-item col-md-6 col-xs-12'>";
         t+="<div class='pi-item-title'>"+data.title+"</div>";
         t+="<div class='pi-item-description'>"+data.description+"</div>";
         if(data.date!=undefined){
             t+="<div class='pi-item-date'>"+data.date+"</div>";
         }
-        t+="<div class='pi-item-content container'>";
+        t+="<div class='pi-item-content'>";
         for(var i=0;i<data.images.length;i++){
-            t+="<div class='pi-images container'>";
-            t+="<img class='row' src='"+data.images[i].src+"'/>";
-            t+="<span class='pi-image-text row'>"+data.images[i].name;
+            t+="<div class='pi-images'>";
+            t+="<img class='col-xs-11' src='"+data.images[i].src+"'/>";
+            t+="<span class='pi-image-text col-xs-11'>"+data.images[i].name;
             if(data.images[i].date!=undefined){
                 t+=", "+data.images[i].date;
             }
--- website/data/resume.js
+++ website/data/resume.js
@@ -7,7 +7,7 @@
     tags:"ASP.NET 4 / Winforms / C# / LINQ / jQuery / Javascript / REST / Angular / XML / XSLT / WPF / Nunit / Ninject / Visual Studio 2010 / Resharper / SQL 2008 r2 / IIS7.5 / Windows Server / Windows Services / asynchronous development / Oracle / MSSQL / MSMQ / CSS3 / HTML",
     content:"Helped update an integral internal windows application which gathered search results and statistics from various social APIs for an industry leading company. Utilized unit testing and solid principles and helping with integration of a branching, feature based, development structure. After a few years out of windows forms application development got up to speed quickly, utilising parallel programming, reporting and user interface design with WPF. Created a HTML5 SPA for elastic search integration using Angular and bootstrap and WebAPI  (REST).",
     fromDate:"May 2014",
-    toDate:"Decemebr 2014"
+    toDate:"December 2014"
     },
     {
     id:"2",
@@ -23,5 +23,66 @@
     content:"Joined a friendly team of developers to push forward an Email alerting system which ran as a modularised WCF system hooking into a SOLR search engine.  Helped junior developers raise their understanding of the opportunities in new development practices and how to utilise Subversion and Microsoft Test Framework to create a formalised branching feature driven SOLID development process.",
     fromDate:"December 2013",
     toDate:"March 2014"
+    },
+    {
+    id:"3",
+    title:"Asp.Net WCF developer (C#, WCF4, MVC,  SQL2008R2, Ninject)",
+    client:"Smart421, London",
+    description:"WCF Enterprise service cluster",
+    tags:"ASP.NET 4 / WCF / X509 CERT/ C# / SQL 2008R2  / LINQ / Javascript / jQuery / XML / WSDL / XHTML / MVC3/4 / KendoUI / XMLSpy / Nunit / Visual Studio 2010 / Resharper / SQL 2008 r2 / IIS7.5 / SoapUI / Active Directory / Windows Server / Windows Services",
+    content:"Joined a large team creating an internal booking, management, reporting and payment processing system for the Airline industry built with .Net 4.0 WCF, MVC3/4, AJAX and SQL Server 2008R2.   Used TFS, SoapUI, XMLSpy, KendoUI, TDD, Unit Testing with Microsoft testing framework, code coverage as part of a large, varied team, to produce a PCI secure financial system for a large multinational Airline.",
+    fromDate:"November 2012",
+    toDate:"November 2013"
+    },
+    {
+    id:"4",
+    title:"Asp.Net Facebook Developer  (C#, WebApi, Facebook,  .Net 4.5, Webservices,  AJAX,  Jquery, MustacheJS, BackboneJS, REST)",
+    client:"Suburb, London",
+    description:"MVC4 WebAPI and javascript Single Page Facebook Application",
+    tags:"ASP.NET 4 / Facebook Open Graph / C# / Entity Framework / LINQ / Javascript / jQuery / MustacheJS / BackboneJS / REST / XHTML / MVC4 / Nunit / Visual Studio 2010 / Resharper / SQL 2008 r2/ IIS7.5 / SOLR / Windows Server",
+    references:[
+        {name:"Danny Daley",
+        position:"Managing Partner",
+        content:"Neil worked with Suburb for a period of 4 weeks to develop a complex Facebook application for one of our clients. During that time I found him to be a dedicated and diligent developer, writing reliable and maintainable code. Neil was always prepared to go the extra mile in order to deliver the project to the highest standards."
+    }],
+    content:"Single Page Application for Facebook allowing visitors to the Adidas Football site to sync workouts completed using Adidas’ miCoach pedometer dongle with workouts completed by famous Adidas Football stars.",
+    fromDate:"September 2012",
+    toDate:"October 2012"
+    },
+    {
+    id:"5",
+    title:"Asp.Net developer (.Net / MVP / REST / Ajax / Entity Framework / Tridion 2011 / Audience Manager / UGC / AJAX / Facebook)",
+    client:"Sapient Nitro, London",
+    description:"WCF/ MVP WCF REST CMS connecting to Tridion and SOLR",
+    tags:"ASP.NET 4 / C# / Entity Framework / Tridion 2011 / ODATA / LINQ / Facebook / Javascript / jQuery / XHTML / MVC3 / MVPWebforms / Rhino / Nunit / Microsoft testing framework / Visual Studio 2010 / Resharper / SQL 2008 / IIS7.5 / SOLR / Tomcat / Windows Server",
+    content:"Worked with Tridion and Front End developers to create a global Tridion based system for a large multinational corporation.<br/><br/>Utilising Tridion API and odata services to deliver audience management, facebook integration and SOLR based search functionality. Used MVP to create an easily expandable toolset for re-use across multiple Tridion Projects. Maintained Tridion DWTs, TBBs and Component Templates.<br/><br/>Utilised jQuery AJAX to enable client page refreshing and XSLT for data transformation.<br/>Implemented Facebook based authentication, married with Tridion Audience Manager Contact Management.Used Tridion odata and UGC to create rating system which automatically updated search results via SOLR.<br/><br/>",
+    fromDate:"April 2012",
+    toDate:"August 2012"
+    },
+    {
+    id:"6",
+    title:"ASP.Net developer (MVC3/Ajax/XML/Entity Framework)",
+    client:"Parexel, London",
+    description:"eLearning Javascript driven application for Pharmaceutical sector",
+    tags:"ASP.NET 4 / C# / Entity Framework / TDD / LINQ / Javascript / jQuery / REST / XHTML / MVC3 / Rhino / Nunit / Microsoft testing framework / Visual Studio 2010 / Resharper / ionic zip / jwplayer",
+    content:"Created a full Learning CMS system which used MVC3 and AJAX to deliver a reusable learning system for slideshows, video, question content and personalised PDF Certificates. Utilising XML for the content manifests and SQL2008 for logging and user management.<br/>Project began with an extensive period of TDD for core functionality and Data processing before moving towards Ajax and javascript for smooth client side content delivery.",
+    fromDate:"February 2012",
+    toDate:"April 2012",
+    references:[
+        {name:"Owen Hall",
+        position:"Delivery Manager",
+        content:"Neil demonstrated an excellent understanding of the requirements for the project that we engaged his services for, and provided a very strong and configurable code-base for us to deploy into production; which has been very well received by our Client. This was backed up with good documentation and a solid technical platform for the development process, that gave us great confidence in Neil’s ability to fulfil our requirements."
+    }]
+    },
+    {
+    id:"7",
+    title:"ASP.NET Developer (Visual Studio 2010 / .Net 4 / WCF / WIF / Azure / SQL 2008r2 / SqlAzure)",
+    client:"IRIS Accountancy, London",
+    description:"Azure based cloud storage system, implementing Windows Identity Foundation and Windows Communication Framework",
+    tags:"ASP.NET  /  C# / Entity Framework / LINQ / DataModel  /  Javascript  / jQuery / HTML / SSL / WCF / WIF / Azure / Sql Azure / MVC3",
+    content:"Helped create a fully cloud based system for the secure sharing of files between Accountants and Clients. Implementing Membership Services and using Windows Communication Framework (WCF) and Windows Identity Framework (WIF) and ASP.Net Membership. Fully Azure Cloud based, implementing Azure Watch for automatic scaling.<br/>Integrating with desktop based software to allow uploads and downloads via WCF or alternatively, a MVC3 website.",
+    fromDate:"May 2011",
+    toDate:"December 2011"
     }
+
 ]    
